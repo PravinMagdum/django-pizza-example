a9a18ae14442c01c06b469b7e6cf4d22f0420083
PravinMagdum <pravin.magdum@gmail.com>
2020-10-01 14:10:32 +0530

    working

--- sonar_configure.groovy
+++ sonar_configure.groovy
@@ -1,40 +1,38 @@
-import hudson.model.*;
-import jenkins.model.*;
 import hudson.tools.*;
 import hudson.plugins.sonar.*;
-// import hudson.plugins.sonar.model.TriggersConfig
+import hudson.plugins.sonar.model.TriggersConfig;
+import hudson.util.Secret;
+
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
+
 
   
 
-  def sonar_name = "sonar-pm"
+  def sonar_name = "sonar-pm1"
   
   def sonar_server_url = "http://3.7.154.171:9000"
-  def sonar_auth_token = "testtoken"
+  def sonar_auth_token = "global-sonartoken"
+def sonar_triggers = new TriggersConfig()
 
   def instance = Jenkins.getInstance()
-def jenkins1 =jenkins.model.Jenkins.get();
 
-
- def test= Jenkins.getDescriptor(SonarGlobalConfiguration)
- 
-
-    sleep(1000)
-    println("Configuring SonarQube...")
+  println("Configuring SonarQube...")
 
     // Get the GlobalConfiguration descriptor of SonarQube plugin.
     def SonarGlobalConfiguration sonar_conf = instance.getDescriptor(SonarGlobalConfiguration.class)
+    
+   def webhooks = Secret.fromString('sdfsdfs')
    
      
     def sonar_inst = new SonarInstallation(
         sonar_name,
-        sonar_server_url,
-      '',
-        sonar_auth_token,
-      '',
+        sonar_server_url
+        ,sonar_auth_token,webhooks,
+        
         '',
         '',
-        '',
-        ''
+        '','',
+      sonar_triggers
     )
     
     def sonar_installations = sonar_conf.getInstallations()
@@ -44,4 +42,3 @@ def jenkins1 =jenkins.model.Jenkins.get();
     
     instance.save()
   
-    
