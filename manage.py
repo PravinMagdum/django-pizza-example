c828a041d3e00260d0a0c69e1af5dcea8d755ae7
PravinMagdum <pravin.magdum@gmail.com>
2020-10-06 15:51:23 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         from configurations.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
+            "Couldn't import Django. Are you sure it's innnstalled and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
